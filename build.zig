193a4b6ac5cdd7bbbf1f0a1240f2a527e5854ae0
darryl west <darryl.west@raincitysoftware.com>
2025-09-22 13:10:16 -0700

    updates

--- build.zig
+++ build.zig
@@ -2,7 +2,15 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+<<<<<<< HEAD
     const optimize = b.standardOptimizeOption(.{});
+=======
+    // For libraries, optimization is typically handled by the consumer
+    // It's also possible to define more custom flags to toggle optional features
+    // of this build script using `b.option()`. All defined flags (including
+    // target and optimize options) will be listed when running `zig build --help`
+    // in this directory.
+>>>>>>> main
 
     // Create the library
     const lib = b.addStaticLibrary(.{
@@ -12,6 +20,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+<<<<<<< HEAD
     // Install the library
     b.installArtifact(lib);
 
@@ -60,4 +69,29 @@ pub fn build(b: *std.Build) void {
         .install_subdir = "docs",
     });
     docs_step.dependOn(&install_docs.step);
+=======
+    // Creates a test executable that will run `test` blocks from the module.
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+
+    // A run step that will run the test executable.
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
+    // A top level step for running all tests.
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_mod_tests.step);
+
+    // Just like flags, top level steps are also listed in the `--help` menu.
+    //
+    // The Zig build system is entirely implemented in userland, which means
+    // that it cannot hook into private compiler APIs. All compilation work
+    // orchestrated by the build system will result in other Zig compiler
+    // subcommands being invoked with the right flags defined. You can observe
+    // these invocations when one fails (or you pass a flag to increase
+    // verbosity) to validate assumptions and diagnose problems.
+    //
+    // Lastly, the Zig build system is relatively simple and self-contained,
+    // and reading its source code will allow you to master it.
+>>>>>>> main
 }
